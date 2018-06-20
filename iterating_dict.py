512eef87dab6c7cbce8f155297ff924229b1d074
aastha3 <duaaastha3@gmail.com>
2018-06-20 17:48:05 +0800

    Update iterating_dict.py

--- iterating_dict.py
+++ iterating_dict.py
@@ -1,2 +1,7 @@
 for k in numbers:
     print("{} = {}".format(k, numbers[k]))
+
+    
+for planet, initial in planet_to_initial.items():
+    print("{} begins with \"{}\"".format(planet.rjust(10), initial))    
+    
