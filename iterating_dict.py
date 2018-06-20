4cf2f6452a5521472a373c4bfc0060ffaf395629
aastha3 <duaaastha3@gmail.com>
2018-06-20 19:06:26 +0800

    Update iterating_dict.py

--- iterating_dict.py
+++ iterating_dict.py
@@ -5,3 +5,11 @@ for k in numbers:
 for planet, initial in planet_to_initial.items():
     print("{} begins with \"{}\"".format(planet.rjust(10), initial))    
     
+ def word_search(documents, keyword):
+    indices = []
+    for i, doc in enumerate(documents):
+        tokens = doc.split()
+        normalized = [token.rstrip('.,').lower() for token in tokens]
+        if keyword.lower() in normalized:
+            indices.append(i)
+    return indices   
