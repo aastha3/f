e6540bd055d58c06b219f58b808546f7e2c283b1
aastha3 <duaaastha3@gmail.com>
2018-06-24 16:43:51 +0800

    Update temp_safe_url.py

--- temp_safe_url.py
+++ temp_safe_url.py
@@ -1,3 +1,14 @@
 import secrets.py 
 
 url = 'https://mydomain.com/reset=' + token_urlsafe()
+
+x[2,-1]   # how to reference an array 
+xlist[1][-1]   # how to reference a list 
+
+
+convert list to array 
+x = numpy.asarray(xlist)
+
+
+works  = array + 10 # 10 added to all elements of the array 
+doesnt = rolls + 10 # for a list an error is thrown   `
